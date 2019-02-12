a632131637647235d6ad768dac82e222fe71b2eb
DESKTOP-P0J49IK\Jelena <jellena.pavlovic@gmail.com>
2019-02-12 11:17:38 +0100

    age till retirement calculator js practice

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,25 @@
 function calculate(age) {
 
-    var age = document.form.input.value;
-    var retirementAge = 65;
-    var alertField = document.getElementById("js-alert");
+    age = document.getElementById("input").value;
+    let output = document.getElementById("output");
+    let retirementAge = 65;
+    let alertField = document.getElementById("js-alert");
     if (age > 100) {
         alertField.innerHTML = "Invalid number, enter numbers from 18 to 99";
-
+        output.value = "";
     } else if (age > 65 && age <= 100) {
         alertField.innerHTML = "You should be retired for " + (age - retirementAge) + " years";
-        document.form.output.value = "";
+        output.value = "";
     } else if (age == 65) {
-       alertField.innerHTML = "Congratulations!! You deserve it!";
-       document.form.output.value = "Retired!";
-    } else if(age<18){
-        alertField.innerHTML= "You are too young to think of retirement!"
-        document.form.output.value = "";
+        alertField.innerHTML = "Congratulations!! You deserve it!";
+        output.value = "Retired!";
+    } else if (age < 18) {
+        alertField.innerHTML = "You are too young to think of retirement!"
+        output.value = "";
     }
-     else {
-        alertField.innerHTML="Keep up the god work! You'll rest later!";
-        var calc = retirementAge - age;
-        document.form.output.value = calc;
+    else {
+        alertField.innerHTML = "Keep up the god work! You'll rest later!";
+        let calc = retirementAge - age;
+        output.value = (" " + calc + " ");
     }
 }

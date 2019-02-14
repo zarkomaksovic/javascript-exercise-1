0586f859e1a70d924b2e60323e46d90eb4714977
DESKTOP-P0J49IK\Jelena <jellena.pavlovic@gmail.com>
2019-02-14 14:17:48 +0100

    country select added

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,48 @@
 function calculate(age) {
-
+    // elements to work with
     age = document.getElementById("input").value;
     let output = document.getElementById("output");
-    let retirementAge = 65;
     let alertField = document.getElementById("js-alert");
-    if (age > 100) {
+    let select = form.select;
+    var country = select[select.selectedIndex].innerHTML;
+    let retirementAge = select[select.selectedIndex].value;
+
+    // CONDITIONS
+    
+    if (age == "") {
+        // user input empty
+        false;
+    } else if (age > 100) {
+        // user age over 100
         alertField.innerHTML = "Invalid number, enter numbers from 18 to 99";
         output.value = "";
-    } else if (age > 65 && age <= 100) {
-        alertField.innerHTML = "You should be retired for " + (age - retirementAge) + " years";
+    } else if (age > retirementAge && age < 100) {
+        // user already retired
+        alertField.innerHTML = alertField.innerHTML = "Retirement age in " + country
+            + " is " + retirementAge + " years." + "</br>"
+            + "You should be retired for " + (age - retirementAge)
+            + " years";
         output.value = "";
-    } else if (age == 65) {
-        alertField.innerHTML = "Congratulations!! You deserve it!";
+    } else if (age == retirementAge) {
+        // retire age equal to user age
+        alertField.innerHTML = alertField.innerHTML = "Retirement age in " + country
+            + " is " + retirementAge + " years." + "</br>"
+            + "Congratulations!! You deserve it!";
         output.value = "Retired!";
+        output.style.backgroundColor="#a2c5ab"
     } else if (age < 18) {
-        alertField.innerHTML = "You are too young to think of retirement!"
+        // user under age limit
+        alertField.innerHTML = alertField.innerHTML = "Retirement age in " + country
+            + " is " + retirementAge + " years." + "</br>"
+            + "You are too young to think of retirement!"
         output.value = "";
-    }
-    else {
-        alertField.innerHTML = "Keep up the god work! You'll rest later!";
+    } else {
+        // calculate age
+        alertField.innerHTML = "Retirement age in " + country
+            + " is " + retirementAge + " years." + "</br>"
+            + "Keep up the god work! You'll rest later!";
         let calc = retirementAge - age;
         output.value = (" " + calc + " ");
+        output.style.backgroundColor="#a2c5ab"
     }
 }
